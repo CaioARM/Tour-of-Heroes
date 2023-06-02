a8ee5dc643aa51a42e1359d4fee71282f39000ae
CaioARM <135237489+CaioARM@users.noreply.github.com>
2023-06-02 14:52:44 -0300

    Add files via upload

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,7 +1,14 @@
 export interface Hero {
     id: number;
     name: string;
- 
+    power: string;
+    tname: string;
+    gen: String;
+    des: String;
+    rank: number;
+    weakness: string;
+    strenght: string;
+
   }
 
   
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,10 +1,10 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'HULK' },
-  { id: 13, name: 'THOR' },
-  { id: 14, name: 'HOMEM DE FERRO' },
-  { id: 15, name: 'CAPITAO AMERICA' },
-  { id: 16, name: 'VIUVA NEGRA' },
-  { id: 17, name: 'GAVIAO ARQUEIRO' },
+  { id: 12, rank: 1, weakness: 'AUTO-CONTROLE', strenght: 'RESILIÊNCIA', name: 'HULK', power: 'FORÇA, RESISTÊNCIA À RADIAÇÃO, RESILIÊNCIA', tname: 'Bruce Banner', gen: 'M', des: 'Não deixe ele com raiva' },
+  { id: 13, rank: 2, weakness: 'SONO DE ODIN', strenght: 'EXPERIÊNCIA',  name: 'THOR', power: 'FORÇA, VOAR, VELOCIDADE', tname: 'THOR', gen: 'M', des: 'Um deus' },
+  { id: 14, rank: 3, weakness: 'ARROGÂNCIA', strenght: 'INTELIGÊNCIA', name: 'HOMEM DE FERRO', power: 'Dinheiro', tname: 'Tony Stark', gen: 'M', des: 'Eu sou o homem de ferro' },
+  { id: 15, rank: 4, weakness: 'DEPENDE DO ESCUDO', strenght: 'LIDERANÇA', name: 'CAPITAO AMERICA', power:'Super Soldado', tname: 'Joao', gen: 'M', des: 'Ele é mais velho que você' },
+  { id: 16, rank: 5, weakness: 'FALTA DE EMPATIA', strenght: 'BELEZA, ARTES MARCIAIS, AMBIÇÃO', name: 'VIUVA NEGRA', power: 'Agilidade', tname: 'Joao', gen: 'F', des: 'A única vingadora mulher' },
+  { id: 17, rank: 6, weakness: 'MEDO DE ALTURA', strenght: 'MEDO DE ALTURA', name: 'GAVIAO ARQUEIRO', power: 'Precisão', tname: 'Joao', gen: 'M', des: 'Quem é ele mesmo?' }
 ];
