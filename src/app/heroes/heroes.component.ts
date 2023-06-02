741761960d0fb5a437eb2196300c6849fb15cdff
CaioARM <135237489+CaioARM@users.noreply.github.com>
2023-06-02 12:15:57 -0300

    Add files via upload

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,6 +15,7 @@ export class HeroesComponent {
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
+  
   ngOnInit(): void {
     this.getHeroes();
   }
@@ -26,5 +27,17 @@ export class HeroesComponent {
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
+  }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
   }
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,10 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 12, name: 'HULK' },
+  { id: 13, name: 'THOR' },
+  { id: 14, name: 'HOMEM DE FERRO' },
+  { id: 15, name: 'CAPITAO AMERICA' },
+  { id: 16, name: 'VIUVA NEGRA' },
+  { id: 17, name: 'GAVIAO ARQUEIRO' },
 ];
